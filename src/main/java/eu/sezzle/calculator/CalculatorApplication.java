c80d47e78fcc5a98eb10c3b28dbd6f75ff84264b
ystanwar <ystanwar512@gmail.com>
2021-02-13 02:37:38 +0530

    basic calculator code with tests

--- src/main/java/eu/sezzle/calculator/CalculatorApplication.java
+++ src/main/java/eu/sezzle/calculator/CalculatorApplication.java
@@ -10,7 +10,7 @@ public class CalculatorApplication {
 
   public static void main(String[] args) {
     //this method will call our calculator
-    //NOTE: only limitation that our calculator has, we have to give space separated expression
+    //NOTE: only limitation that our calculator has is we have to give space separated expression
     //correct ex: 2 + 3 or 5 / 2 - 3
     //incorrect ex: 2+3 or 5/2-3 will be considered invalid
     calculateExpression();
